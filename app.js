198f7b864f438af3c3a76995a93050f937ffde14
zxcvbnius <zxcvbnius@gmail.com>
2016-04-20 23:53:17 +0800

    fixed log null bug

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var _ = require('lodash'),
             if(chat.id === message.chat.id && Pi.serial !== message.senderUser.serial) { // only handle message in chat
                 var msg = message.data
                 var cmd = commands.get(msg)
-                log('command is: ' + cmd.text)
-                if(cmd) { cmd.action(socket) }
+                if(cmd) { log('command is: ' + cmd.text); cmd.action(socket) }
             }
         })
     })
